4ed26199cfb9216d9d8911d2d9b142a44b1a6d03
000xzhou <xzhou92@gmail.com>
2025-05-19 15:18:36 -0400

    added pending refund list to dashboard.

--- frontend/vite-project/src/components/employee/customers/Refund.jsx
+++ frontend/vite-project/src/components/employee/customers/Refund.jsx
@@ -31,14 +31,48 @@ function Refund() {
     "/customers/:customerId/orders???"
   );
 
+  const formatPrice = (price) =>
+    price.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
   if (loading) <div>loading...</div>;
 
-  console.log(refundApi);
   return (
     <div>
       <h2>Pending Refunds</h2>
-      {refundApi?.length > 0 &&
-        refundApi.map((refund) => <div key={refund._id}>{refund._id}</div>)}
+      {refundApi?.length > 0 ? (
+        refundApi.map((refund) => (
+          <div key={refund._id}>
+            <h3>Order #{refund.orderId._id}</h3>
+            <div>
+              <p>Customer ID: {refund.customerId._id}</p>
+              <p>
+                Requested: {new Date(refund.requestedAt).toLocaleDateString()}
+              </p>
+              <p>Total Refund: {formatPrice(refund.amount)}</p>
+            </div>
+            <div>
+              {refund.items.map((item) => (
+                <div key={item.itemId}>
+                  <img src={item.productImage} alt="Product image" />
+                  <div>
+                    <p>Product ID: {item._id}</p>
+                    <p>Product: {item.productId.name}</p>
+                    <p>Qty: {item.quantity}</p>
+                    <p>Reason: {item.reason}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <button>Approve</button>
+            <button>Reject</button>
+          </div>
+        ))
+      ) : (
+        <div>No pending refunds</div>
+      )}
     </div>
   );
 }
